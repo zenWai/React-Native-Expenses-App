c238e10fd4574885a331cef8a42e3b133bc84716
Fernando <ivopresa@gmail.com>
2023-06-27 17:31:32 +0200

    custom icon, +

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
 import {GlobalStyles} from "./constants/styles";
 import {Ionicons} from "@expo/vector-icons";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -18,6 +19,9 @@ function ExpensesOverview() {
             headerTintColor: 'white',
             tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
             tabBarActiveTintColor: GlobalStyles.colors.accent500,
+            headerRight: ({tintColor}) => (
+                <IconButton icon="add" size={24} color={tintColor} onPress={()=>{}}/>
+            ),
         }}
         >
             <BottomTabs.Screen
