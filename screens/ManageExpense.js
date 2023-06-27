d23585df9529b7d5f5d29955a5cd0d09458512bb
Fernando <ivopresa@gmail.com>
2023-06-27 20:07:36 +0200

    ManageExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -18,15 +18,15 @@ function ManageExpense({ route, navigation }) {
     }, [navigation, isEditing]);
 
     function deleteExpenseHandler() {
-
+        navigation.goBack();
     }
 
     function cancelHandler() {
-
+        navigation.goBack();
     }
 
     function confirmHandler() {
-
+        navigation.goBack();
     }
 
     return (
