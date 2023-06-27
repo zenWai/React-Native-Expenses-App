78cf39362e648de22be7bcc51258f61fa887f1ba
Fernando <ivopresa@gmail.com>
2023-06-27 18:29:04 +0200

    Custom Buttons ManageExpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
-import {Text} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {useLayoutEffect} from "react";
+import IconButton from "../components/UI/IconButton";
+import {GlobalStyles} from "../constants/styles";
+import Button from "../components/UI/Button";
 
 function ManageExpense({ route, navigation }) {
     //check if we are editing item or adding
@@ -14,7 +17,60 @@ function ManageExpense({ route, navigation }) {
         });
     }, [navigation, isEditing]);
 
-    return <Text>Manage Expense Screen</Text>
+    function deleteExpenseHandler() {
+
+    }
+
+    function cancelHandler() {
+
+    }
+
+    function confirmHandler() {
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode="flat" onPress={cancelHandler}>Cancel</Button>
+                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+
+        </View>
+    );
 }
 
+const styles = StyleSheet.create({
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center',
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
+    },
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800,
+    },
+})
 export default ManageExpense;
