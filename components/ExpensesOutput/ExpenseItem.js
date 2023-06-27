080d90e3eeada76893738f72a334e92e88bb724e
Fernando <ivopresa@gmail.com>
2023-06-27 17:58:34 +0200

    Manage Screen is editing price or adding?

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -3,10 +3,13 @@ import {useNavigation} from "@react-navigation/native";
 import {GlobalStyles} from "../../constants/styles";
 import {getFormattedDate} from "../../util/date";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
     const navigation = useNavigation();
+
     function ExpensePressHandler() {
-        navigation.navigate('ManageExpense');
+        navigation.navigate('ManageExpense', {
+            expenseId: id,
+        });
     }
 
     return (
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,19 @@
 import {Text} from "react-native";
+import {useLayoutEffect} from "react";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+    //check if we are editing item or adding
+    //from where is screen getting called?
+    //If params is undefined we are adding
+    const editedExpenseId = route.params?.expenseId;
+    const isEditing = !!editedExpenseId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        });
+    }, [navigation, isEditing]);
+
     return <Text>Manage Expense Screen</Text>
 }
 
